4057ae16b92def31dfae814d942e0e818e92a7e6
Jeff Langdon <jeff.beyer@gmail.com>
2024-12-31 14:56:11 -0800

    add arg checking

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"os"
+)
 
-    print("hello")
+func main() {
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(args) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	baseURL := args[0]
+
+	fmt.Printf("starting crawl of: %v\n", baseURL)
 }
