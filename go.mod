a82fc3a7cbf70432ae295704e39251cef88170cb
Jeff Langdon <jeff.beyer@gmail.com>
2024-12-31 14:49:20 -0800

    adds url from html gathering

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/geophpherie/boot-dev-web-crawler-go
 
 go 1.23.4
+
+require golang.org/x/net v0.33.0
--- normalize_url.go
+++ normalize_url.go
@@ -3,6 +3,9 @@ package main
 import (
 	"net/url"
 	"strings"
+
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 )
 
 func normalizeURL(inputUrl string) (string, error) {
@@ -23,7 +26,35 @@ func normalizeURL(inputUrl string) (string, error) {
 	return outUrl, nil
 }
 
-//
-// func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
-//
-// }
+func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
+	htmlReader := strings.NewReader(htmlBody)
+
+	document, err := html.Parse(htmlReader)
+	if err != nil {
+		return []string{}, err
+	}
+
+	var links []string
+	for node := range document.Descendants() {
+		if node.Type == html.ElementNode && node.DataAtom == atom.A {
+			for _, a := range node.Attr {
+				if a.Key == "href" {
+					var newLink string
+					if a.Val[0] == '/' {
+						newLink, err = url.JoinPath(rawBaseURL, a.Val)
+						if err != nil {
+							return []string{}, err
+						}
+					} else {
+						newLink = a.Val
+					}
+					links = append(links, newLink)
+					break
+				}
+			}
+
+		}
+	}
+
+	return links, nil
+}
